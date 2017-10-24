b504ca2c0df3f9fb2613b4e45ecf2948d3d6e0ef
Mateus Valgueiro Teixeira <mvt@ug4c22.windows.cin.ufpe.br>
2017-10-24 17:54:11 -0300

    adding some sympy things

--- euler.py
+++ euler.py
@@ -1,30 +1,27 @@
+from __future__ import division
 from sympy import * 
-def euler(f): 
+
+
+def euler(expr): 
     t0 = int(input("Digite o numero correspondente a T0:"))
     y0 = int(input("Digite o numero correspondente a y0:"))
     h = int(input("Digite o numero correspondente ao tamanho dos passos:"))
     n = int(input("Digite o numero correspondente ao numero de passos:"))
 
     for j in range(1, n):
-        print ("We're on time %d" % (j))
-        #y = f(j)    
-        #print (y + '\n')
+        res = expr.subs([y, n], [t, tAux])
+        print (res)
     return
 
 def main():
-    # f = lambda x: eval(input())
-    euler(5)
+    y, t, x = symbols('y t x');
+    str_expr = input("Digite sua F(y,t):\n");
+    expr = sympify(str_expr);
+    
+    res = expr.subs([(y, 1), (t, 3)]);
+    print ("Para F(1,3):");
+    print (res);
 
-
-#defina f(t,y)
-#Passo 2. alimente os valores iniciais 10 e y0
-#Passo 3. alimente o tamanho do passo h e o ntImero de passos n
-# Passo 4. escreva tO e y0
-# Passo 5. para j de 1 ate n calcule
-# Passo 6. k 1 = f (t,y)
-# y=y+h*kl
-# t =t +h
-# Pass() 7. escreva*/
 
 if __name__ == "__main__":
      main()
