3ebef4934bcf695d9fb53589607591e86acca162
Valgueiro <mvt@cin.ufpe.br>
2017-11-05 21:55:16 -0300

    Adding rk and starting README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # projetoMetodos
 Todos os projetos feitos por mim na disciplina de Metodos Numericos
+
+## 	Informações Gerais
+
+#### Dependencias:
+Para rodar este programa você deve estar em um ambiente ***Linux*** e com a biblioteca [Simpy](http://docs.sympy.org/latest/install.html) e [matplotLib](https://matplotlib.org/users/installing.html) instaladas em ssua máquina.
+
+#### Compilando e rodando o server:
+Assim, quando quiser executá-lo, com todas as dependências devidamente instaladas, você deve rodar o seguinte comando: 
+	$ python motodos.py
+
--- metodos.py
+++ metodos.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-import matplotlib.pyplot as plt
+#import matplotlib as plt
 from sympy import * 
-y, t, x = symbols('y t x');
+y, t, x, g = symbols('y t x g');
 
 def F(expr, yn, tn):
     return expr.subs([(y, yn), (t, tn)])
 
-def euler(expr, t0, y0, h, n):     #ok
+def euler(expr, t0, y0, h, n):
     print ("T = " + str(t0) + "   Y = " + str(y0))
     
     vEulerx = []
@@ -25,7 +25,7 @@ def euler(expr, t0, y0, h, n):     #ok
 
     return
 
-def eulerInverso(expr, t0, y0, h, n):     
+def eulerInverso(expr, t0, y0, h, n):
     print ("T = " + str(t0) + "   Y = " + str(y0))
     
     yNow = y0
@@ -64,12 +64,43 @@ def eulerModificado(expr, t0, y0, h, n):
     return
 
 
+def rk(expr, t0, y0, h, n):
+    print ("T = " + str(t0) + "   Y = " + str(y0))
+    
+    yNow = y0
+    tNow = t0
+
+    for j in range(1, n):
+        half = 0.5 * h
+        k1 = F(expr, yNow, tNow);
+        k2 = F(expr, yNow + half * k1, tNow + half)
+        k3 = F(expr, yNow + half * k2, tNow + half)
+        k4 = F(expr, yNow + h * k3, tNow + h)
+
+        yNow = yNow + (h * ((k1 + (2 * k2) + (2 * k3) + k4)/6))
+        tNow = tNow+h
+                 
+        if(j%(0.1/h)==0):
+            print ("T = " + str(tNow) + "   Y = " + str(yNow));
+
     return
 
 def main():
-    str_expr = input("Digite sua F(y,t):\n")
-    expr = sympify(str_expr)
+    # str_expr = input("Digite sua expressao:\n")
+    # expr = lambdify((t,y), solve(sympify(str_expr), g).pop());
+    # print(str(expr))
+    #return
+    
+    print("Digite o metodo que você deseja calcular:")
+    print("1 - Euler Simples")
+    print("2 - Euler Inverso")
+    print("3 - Euler Modificado")
+    print("4 - Runge-Kutta")
+    method = int(input("Sua escolha:"))
 
+
+    str_expr = input("Digite sua expressao:\n")
+    expr = sympify(str_expr)
     t0 = float(input("Digite T0:"))
     y0 = float(input("Digite y0:"))
     h = float(input("Digite o tamanho dos passos:"))
@@ -80,26 +111,34 @@ def main():
     # t0 = 0
     # y0 = 1
     # tf = 2
-    # h = 0.025    
+    # h = 0.05   
 
-    # n = int((tf-t0)/h) + 1
+    n = int((tf-t0)/h) + 1
     
-    print("Valores calculados por Euler simples:\n");
-    euler(expr, t0, y0, h, n)
-
+    if (method == 1):
+        print("Valores calculados por Euler simples:\n");
+        euler(expr, t0, y0, h, n)
+        #plt.title("Euler Simples")
+    elif (method == 2):
+        print("\nValores calculados por Euler inverso:\n");
+        eulerInverso(expr, t0, y0, h, n)
+       # plt.title("Euler Inverso")
+    elif (method == 3):
+        print("\nValores calculados por Euler modificado:\n");
+        eulerModificado(expr, t0, y0, h, n)
+        #plt.title("Euler Modificado")
+    elif (method == 4):
+        print("\nValores calculados por Runge-Kutta:\n");
+        rk(expr, t0, y0, h, n)
+        #plt.title("Runge-Kutta")
+        
     # plt.plot(vx, vy, 'go')
     # plt.plot(vx, vy, 'k:', color='blue')
-    # plt.title("Euler")
+    # 
     # plt.xlabel("t")
     # plt.ylabel("y")
     # plt.show()
-
-    print("\nValores calculados por Euler inverso:\n");
-    eulerInverso(expr, t0, y0, h, n)
-
-    print("\nValores calculados por Euler modificado:\n");
-    eulerModificado(expr, t0, y0, h, n)
-
+    
 
 if __name__ == "__main__":
      main()
